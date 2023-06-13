0c473b719c03ddc810eed1370f0503dc92a73c92
AnnaKru86 <nyura86@mail.ru>
2023-06-13 11:05:20 +0300

    first commit

---
+++ home11.js
@@ -0,0 +1,8 @@
+let a= document.getElementById('price_marakas');
+let b=document.getElementById ('price_pyramid');
+let c=document.getElementById ('price_xylophone');
+
+function sum (){
+result=a+b+c;
+}
+document.getElementById('sum').innerHTML=result;
