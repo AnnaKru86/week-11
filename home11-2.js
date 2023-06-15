29aa57fbfe499f9086763b644fd8660096f11798
Anna Kriuchkova <123774941+AnnaKru86@users.noreply.github.com>
2023-06-15 14:47:47 +0300

    Add files via upload

--- home11-2.js
+++ home11-2.js
@@ -1,6 +1,6 @@
 let a= document.getElementById ("input_1").value;
 let button=document.getElementById ("button");
- button.onclick = function quatro() {
- result=a*a;
+function quatro() {
+result=a*a;
 document.getElementById ("input_2").value=result;
    }      
